ce0c76700c5e380cd7a6b97e68abc4a181dd60ca
gutsy47 <bestcom5055@gmail.com>
2023-05-16 05:19:17 +0300

    Feat: Split string and Expression To RPN
    vector<string> splitString() has been provided. This function splits the expression into operands
    
    vector<string> toRPN() has been provided (w/out Debug Mode, it'll be added ASAP). This function translates an ordinary expression into reverse polish notation. Returns vector of RPN-operands

--- practice03/main03.cpp
+++ practice03/main03.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <iomanip>
 #include <unordered_map>
+#include <vector>
 #include "stack.h"
 
 bool inputAction(char &userAction) {
@@ -24,44 +25,63 @@ bool inputAction(char &userAction) {
     return true;
 }
 
-std::string toRPN(const std::string& expr) {
-    Stack opStack;
-    std::string rpnExpr;
-    std::unordered_map<char, int> precedences {
-            {'+', 1},
-            {'-', 1},
-            {'*', 2},
-            {'/', 2}
-    };
-    for (const char ch : expr) {
-        std::string c(1, ch);
-        if (isdigit(ch)) {
-            rpnExpr += c;
-        } else if (ch == '(') {
-            opStack.push(c);
-        } else if (ch == ')') {
-            while (!opStack.isEmpty() && opStack.top()[0] != '(') {
-                rpnExpr += opStack.top();
-                opStack.pop();
+std::vector<std::string> splitString(std::string str) {
+    std::vector<std::string> operands;
+    char operators[6] = {'(', ')', '+', '-', '*', '/'};
+
+    str.erase(remove(str.begin(), str.end(), ' '), str.end());
+    std::string number;
+    for (char c : str) {
+        std::string s(1, c);
+        auto find = std::find(std::begin(operators), std::end(operators), c);
+        if (find != std::end(operators)) {   // Operator
+            if (!number.empty()) {
+                operands.push_back(number);
+                number.clear();
             }
-            if (opStack.isEmpty()) return "";
-            opStack.pop();
-        } else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
-            while (!opStack.isEmpty() && opStack.top()[0] != '(' && precedences[ch] <= precedences[opStack.top()[0]]) {
-                rpnExpr += opStack.top();
-                opStack.pop();
-            }
-            opStack.push(c);
-        } else return "";
+            operands.push_back(s);
+        } else if (isdigit(c)) {             // Number
+            number += c;
+        } else {                                // Unknown symbol error
+            throw "Unknown symbol in expression";
+        }
     }
-    while (!opStack.isEmpty()) {
-        if (opStack.top()[0] == '(') return "";
-        rpnExpr += opStack.top();
-        opStack.pop();
-    }
-    return rpnExpr;
+    if (!number.empty()) operands.push_back(number);
+    return operands;
 }
 
+std::vector<std::string> toRPN(const std::string& expr, bool isDebugMode = false) {
+    std::vector<std::string> result;
+
+    Stack opStack;
+    std::vector<std::string> operands = splitString(expr);
+    std::unordered_map<std::string, int> precedences {
+            {"+", 1},
+            {"-", 1},
+            {"*", 2},
+            {"/", 2}
+    };
+
+    for (std::string operand : operands) {
+        if (isdigit(operand[0])) result.push_back(operand);            // Number: Add to result immediately
+        else if (operand == "(") opStack.push(operand);             // Open brace: Add to opStack
+        else if (operand == ")") {                                     // Close brace: Read stack while not (
+            while (opStack.top() != "(") result.push_back(opStack.pop());
+            if (opStack.isEmpty()) throw "Brace error"; // No open brace found
+            opStack.pop(); // Delete open brace from stack
+        } else if (precedences.count(operand) > 0) {                // Operator
+            while (!opStack.isEmpty() && opStack.top() != "(" && precedences[operand] <= precedences[opStack.top()])
+                result.push_back(opStack.pop());
+            opStack.push(operand);
+        } else throw "Invalid syntax";
+    }
+    while (!opStack.isEmpty()) {
+        if (opStack.top() == "(") throw "Unclosed brace";
+        result.push_back(opStack.top());
+        opStack.pop();
+    }
+    return result;
+}
 
 int main() {
     bool isDebugMode = false;
@@ -72,8 +92,24 @@ int main() {
 
         switch (userAction) {
             // Infix -> RPN+NPN -> Answer
-            case '1':
+            case '1': {
+
+                std::cout << "<< Expression\n>> ";
+                std::string expr;
+                std::getline(std::cin, expr);
+
+                std::vector<std::string> rpn = toRPN(expr, isDebugMode);
+                // toNPN
+                // countRPN
+                // countNPN
+                // check RPN == NPN
+
+                std::cout << "RPN: ";
+                for (auto operand : rpn) std::cout << operand << ' ';
+                std::cout << std::endl;
+
                 break;
+            }
 
             // RPN -> Answer
             case '2':
